49e31d913d1ec9e65e3fef58ec725da2ddae1736
DeeKahy <97156953+DeeKahy@users.noreply.github.com>
2025-09-11 14:58:00 +0200

    wow stuff works? no no it does not

--- dut/main.py
+++ dut/main.py
@@ -1,7 +1,50 @@
-from flask import Flask
+# Importing required functions 
+from flask import Flask, request
 
+# Flask constructor 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+isAvaialbe = True
+queue = []
+
+# Single URL Converter 
+
+
+@app.get('/api/requesting/<clientno>')
+def requesting(clientno):
+    queue.append(clientno)
+    if not queue:
+        isAvaialbe = False
+        return "okay go right straight totally ahead"
+    
+    isAvaialbe = False
+
+
+
+    return "You tried accessing 'single_converter' \
+    endpoint with value of 'menu' as " + str(clientno)
+
+
+
+@app.get('/api/returning/<clientno>')
+def returning(clientno):
+    if not queue:
+        return "something seriously went wrong go fix anders"
+
+    if clientno == queue[0]:
+        queue.pop(0)
+        if not queue:
+            isAvailable = True
+    else:
+        return "something seriously went wrong go fix anders"
+    
+        
+    return "Returning: " + str(clientno)
+
+
+
+# Main Driver Function 
+if __name__ == '__main__':
+    # Run the application on the local development server
+    app.run(debug=True)
