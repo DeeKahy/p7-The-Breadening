319cb07c58c6909c081bf29e7c1b2f9c622cc850
AquaHippo <nannad2000@gmail.com>
2025-12-12 14:21:31 +0100

    deleted old code

--- dut/WebVersion/main.py
+++ dut/WebVersion/main.py
@@ -15,10 +15,13 @@ app = Flask(__name__)
 class Centralized:
     def __init__(
         self,
+        processes: Dict[int, str],
+        process_id: int,
         isAvailable: bool,
         queue: list[Dict[str, Any]],
         granted_id: int,
         parameters: Dict[str, Any],
+        round_trip_time: float,
     ):
         self.isAvailable = True
         self.queue = []
@@ -125,65 +128,6 @@ class Centralized:
             self.handle_request(sender_id)
         elif msg_type == "done":
             self.handle_done(sender_id)
-
-"""
-    @app.get('/api/requesting/<clientno>')
-    def requesting(clientno):
-        global isAvailable, queue
-
-        if clientno not in queue:
-            queue.append(clientno)
-
-        if queue.index(clientno) == 0:
-            isAvailable = False
-            return jsonify({
-                "message": "proceed",
-                "position": 0
-            }), 200 #"okay go right straight totally ahead"
-        else: #queue.index(clientno) > 0:
-            return jsonify({
-                "message": "queued",
-                "position": queue.index(clientno)
-            }), 202 #"you have now been queued, i will return when client[" + str(queue.index(clientno) - 1 ) +  "] has returned"
-        #else:
-        #    return jsonify({
-        #        "message": "already_in_queue",
-        #        "position": queue.index(clientno)
-        #    }), 409 #"you are already in the queue, please wait until client[" + str(queue.index(clientno) - 1 ) + "] has returned"
-
-
-    @app.get('/api/returning/<clientno>')
-    def returning(clientno):
-        global isAvailable, queue
-
-        if not queue:
-            return jsonify({"message": "queue_empty_error"}), 500 #"something seriously went wrong go fix anders"
-
-        if clientno != queue[0]:
-            if clientno in queue:
-                return jsonify({
-                    "message": "not_your_turn",
-                    "position": queue.index(clientno)
-                }), 423 #"Not your turn yet, please wait until client[" + str(queue.index(clientno) - 1 ) + "] has returned"
-            else:
-                return jsonify({
-                    "message": "not_in_queue"
-                }), 409 #"You are not in the queue"
-        else:
-            queue.pop(0)
-
-            if queue:
-                isAvailable = False
-                return jsonify({
-                    "message": "returned",
-                    "next": queue[0]
-                }), 200 #"Returning: " + str(clientno) + " next is " + str(queue[0])
-            else:
-                isAvailable = True
-                return jsonify({
-                    "message": "queue_empty"
-                }), 204 #"Queue is now empty."
-"""
 
 # --- Flask wiring -----------------------------------------------------------
 
